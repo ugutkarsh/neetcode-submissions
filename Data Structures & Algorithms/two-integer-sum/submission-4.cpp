3755bdb076736ab78afb73e9ac4ec8e8d86195d3
ugutkarsh <106578094+ugutkarsh@users.noreply.github.com>
2026-07-02 14:29:46 -0700

    Bulk sync: 24 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int num = nums.size();
+
+        for(int i =0;i<num;i++){
+            for(int j=i+1;j<num;j++){
+                if((nums[i]+nums[j])==target){
+                    return {i,j};
+
+                }
+            }
+        }
+        return {};
+    }
+};
